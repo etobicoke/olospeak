c36c2ecd1e369e466019fc44b80325f1eec3450f
Imothep <nganso@gmail.com>
2024-02-27 23:41:08 -0500

    refac: minor markup cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,6 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <div>
-        </div>
-      </div>
     </main>
   );
 }
