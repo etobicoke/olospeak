a5cac57067a42a0fe2490c303f05682b7738216d
Imothep <nganso@gmail.com>
2025-01-12 23:40:43 +0100

    feat: minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import '../../styles/globals.scss'
 
+const firstFunction = () => {
+	let myVar = 45;
+	
+	return myVar + Math.random();
+}
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<nav>Navigation</nav>
+				<section>First Section</section>
+			</body>
 		</html>
 	)
 }
